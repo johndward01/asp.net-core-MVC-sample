6ade20247f42b7dcc05ce23fdeddbea6787cd4d5
jdw <johndward01@gmail.com>
2022-01-10 14:29:28 -0600

    Add project files.

---
+++ MVC-DEMO/Controllers/ProductController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_DEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_DEMO.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepo _repo;
+
+        public ProductController(IProductRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+            return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = _repo.GetProduct(id);
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = _repo.GetProduct(id);
+
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            _repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
+
+        public IActionResult InsertProduct()
+        {
+            var prod = _repo.AssignCategory();
+
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            _repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            _repo.DeleteProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+    }
+}
---
+++ MVC-DEMO/IProductRepo.cs
@@ -0,0 +1,19 @@
+﻿using MVC_DEMO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_DEMO
+{
+    public interface IProductRepo
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
